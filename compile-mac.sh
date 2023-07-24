37c55687ef1c0a86728e1ac17223867554d23072
Nimbi <JoshuaVL1988@protonmail.com>
2023-07-24 16:15:06 -0700

    Update compile-mac.sh

--- compile-mac.sh
+++ compile-mac.sh
@@ -4,6 +4,6 @@ USER_AGENT=firefox
 INTERNAL_URLS='.*?'
 ICON='res/icon.png'
 CSS='res/site.css'
-BUILD_PATH='./out'
+BUILD_PATH='out'
 
 nativefier -u $USER_AGENT$ -n $NAME$ $URL$ --tray 'true' --enable-es3-apis --icon $ICON$ --bounce 'true' --darwin-dark-mode-support 'true' --file-download-options '{"saveas": true}' --inject $CSS$ --titlebar-style 'hiddenInsert' --internal-urls $INTERNAL_URLS$ $BUILD_PATH$
