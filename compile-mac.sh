2514e10726c7b3957111154c546a9ef4c9439914
Nimbi <JoshuaVL1988@protonmail.com>
2023-07-24 16:07:17 -0700

    Update compile-mac.sh

--- compile-mac.sh
+++ compile-mac.sh
@@ -5,4 +5,4 @@ INTERNAL_URLS='.*?'
 ICON='res/icon.png'
 BUILD_PATH='./out'
 
-nativefier -u $USER_AGENT$ -n $NAME$ $URL$ --tray 'true' --enable-es3-apis --icon $ICON$ --bounce 'true' --darwin-darkmode-support 'true' --file-download-options '{"saveas": true}' --internal-urls $INTERNAL_URLS$ $BUILD_PATH$
+nativefier -u $USER_AGENT$ -n $NAME$ $URL$ --tray 'true' --enable-es3-apis --icon $ICON$ --bounce 'true' --darwin-dark-mode-support 'true' --file-download-options '{"saveas": true}' --internal-urls $INTERNAL_URLS$ $BUILD_PATH$
