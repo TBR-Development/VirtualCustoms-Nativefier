56d02b700763c4f99fdc0eadb091beb4d0d347e0
Nimbi <JoshuaVL1988@protonmail.com>
2023-07-24 15:51:40 -0700

    Create compile.bat

---
+++ compile.bat
@@ -0,0 +1,9 @@
+SET URL="https://virtualcustoma.net"
+SET NAME="Virtual Customs"
+SET AGENT=firefox
+SET BGCOLOR="#333333"
+SET ICON="res/icon.ico"
+SET INTERNAL_URLS=".*?"
+SET BUILD_PATH="out"
+
+nativefier -u %AGENT% -n %NAME% %URL% --tray "true" --enable-es3-apis --file-download-options "{/"saveas/": true}" --interlal-urls %INTERNAL_URLS% %BUILD_PATH% 
